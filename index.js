468a324f7af86bc0af13d234fa54988c76117ca6
Henrique Ferraz <henriqueoliver9@gmail.com>
2021-12-14 14:06:15 -0300

    Pronto

--- index.js
+++ index.js
@@ -1,24 +1,17 @@
-const botao = $("#btn-in")
-const ttulo = $("#titulo")
-const solucao = $("#imagem")
-const textExplicacao = $("#esplicação")
+$("#btn-in").click(
+    function(){
+        var data = $("#date").val(); 
+        $.ajax({
+            url: `https://api.nasa.gov/planetary/apod?api_key=kb1rtE5VqFl1PAaE8dI16yagmdTXhJQj1ykRqct2&date=${data}`,
+            type: 'GET',
 
-botao.('onclick', function(evento){
-    evento.preventDeFault()
-    sendApiRequest()
-}
-
-function apiNasa(){
-    $.ajax({
-        url:`https://api.nasa.gov/planetary/apod? api_key = NQYH3fThl9tf9JqcaE5aLiBxsfNkHmh302iSCcxo` + $(`#data`).val(),
-        sucesso: function(resultado){
-            obj: resultado,
-            titulo.html(`${obj.title}`)
-            if(obj.media_type = 'video'){
-                resultado.html(`<img width = "600px" heght = "600px" id="result" src="${obj.url}" alt=""></img>`)
-            }else{
-                resultado.html(`<iframe id="video" src="${obj.url}" alt=""></iframe>`)
+            success: function(result){
+                $('#imagem').height("350px")
+                $('#imagem').width("650px")
+                $('#titulo').text(result.title)
+                $('#imagem').attr("src", result.url)
+                $('#esplicação').text(result.explanation)
             }
-        }
-    })
-}
+        })
+    }
+)
